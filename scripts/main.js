45a5b115923f27dee25451bdc1b641752372e65d
everbrez <everbrez@gmail.com>
2018-12-16 20:22:31 +0800

    [index] nav phone ver.

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,17 @@
+window.addEventListener('load', () => {
   navToggle()
-  console.log(23323)
 
   function navToggle() {
     const nav = document.getElementById('nav')
     const showBtn = document.getElementById('nav-show')
     const closeBtn = document.getElementById('nav-close')
-    console.log(showBtn, closeBtn)
+    const mark = document.getElementById('nav-mark')
 
+    mark.addEventListener('click', e => {
+      e.stopPropagation()
+      nav.classList.remove('show')
+    }, true)
     showBtn.addEventListener('click', () => nav.classList.add('show'), false)
     closeBtn.addEventListener('click', () => nav.classList.remove('show'), false)
   }
+}, false)
