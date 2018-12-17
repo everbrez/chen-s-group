9db847f78186646226faa1bf7aa84eeea3b73ba8
everbrez <everbrez@gmail.com>
2018-12-17 11:07:45 +0800

    [script]change main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,17 @@
-window.addEventListener('load', () => {
-  navToggle()
+// window.addEventListener('load', () => {
+//   navToggle()
 
-  function navToggle() {
-    const nav = document.getElementById('nav')
-    const showBtn = document.getElementById('nav-show')
-    const closeBtn = document.getElementById('nav-close')
-    const mark = document.getElementById('nav-mark')
+//   function navToggle() {
+//     const nav = document.getElementById('nav')
+//     const showBtn = document.getElementById('nav-show')
+//     const closeBtn = document.getElementById('nav-close')
+//     const mark = document.getElementById('nav-mark')
 
-    mark.addEventListener('click', e => {
-      e.stopPropagation()
-      nav.classList.remove('show')
-    }, true)
-    showBtn.addEventListener('click', () => nav.classList.add('show'), false)
-    closeBtn.addEventListener('click', () => nav.classList.remove('show'), false)
-  }
-}, false)
+//     mark.addEventListener('click', e => {
+//       e.stopPropagation()
+//       nav.classList.remove('show')
+//     }, true)
+//     showBtn.addEventListener('click', () => nav.classList.add('show'), false)
+//     closeBtn.addEventListener('click', () => nav.classList.remove('show'), false)
+//   }
+// }, false)
